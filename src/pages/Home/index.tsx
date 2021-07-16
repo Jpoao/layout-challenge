ad7f0500a0be34de2b8ca47b357d073ca823008b
João Paulo <mg_ses@hotmail.com>
2021-07-16 02:50:25 -0300

    Home mobile sytles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,22 @@
+import "./styles.css";
+import car from "assets/images/homeCar.png";
 
 const Home = () => {
-    return(
-        <h6>HOME</h6>
-    );
-}
+  return (
+    <div className="home-container">
+      <div className="home-top-container">
+        <img src={car} alt="Home Car" />
+        <h1>O carro perfeito para você</h1>
+        <p>
+          Conheça nossos carros e dê mais um passo na realização do seu sonho
+        </p>
+      </div>
+      <div className="home-botton-container">
+        <button>VER CATÁLOGO</button>
+        <p>Comece agora a navegar</p>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
